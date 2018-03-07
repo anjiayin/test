542f40d69672f952d918f86d94d54d5a1dfb2a2e
zidong <anjiayin@sohu.com>
2018-03-07 22:51:30 +0800

    normal

--- test.py
+++ test.py
@@ -2,4 +2,17 @@ import sys
 import io
 #sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding = 'utf-8')
 
-print('\ub234')
+import requests
+url = 'http://kaoshi.edu.sina.com.cn/college/scorelist?tab=batch&wl=1&local=2&batch=&syear=2013'
+result = requests.get(url)
+print(result.status_code)
+print('_'*40)
+print(result.content.decode('utf-8'))
+print(type(result))
+print(type(result.content))
+print(type(result.content.decode('utf-8')))
+'''
+fw = open('result.txt', 'w')
+fw.write(result._content.decode('utf-8').encode())
+fw.close
+'''
