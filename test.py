90150dc272e6b66f586d5ee5a4fa3eb33ff9f484
zidong <anjiayin@sohu.com>
2018-03-08 22:03:06 +0800

    ok

--- test.py
+++ test.py
@@ -6,21 +6,13 @@ import requests
 url = 'http://kaoshi.edu.sina.com.cn/college/scorelist?tab=batch&wl=1&local=2&batch=&syear=2013'
 result = requests.get(url)
 print(result.status_code)
-print('_'*40)
+print('_' * 40)
 print(result.content.decode('utf-8'))
 print(type(result))
 print(type(result.content))
-<<<<<<< HEAD
 print(type(result.content.decode('utf-8')))
 '''
 fw = open('result.txt', 'w')
 fw.write(result._content.decode('utf-8').encode())
 fw.close
 '''
-=======
-
-fw = open('result.txt', 'w')
-fw.write(result._content.decode('utf-8'))
-fw.close
-
->>>>>>> 136dfd137bd4b17031efbd2fff7c223f7bbfcff9
